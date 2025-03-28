dcec81b17f67367dd901c631e95643c284b3088e
IPVA <ivanpereira345@gmail.com>
2025-03-28 12:20:28 -0300

    Initial commit

---
+++ website-test/src/test/java/com/sakundo/website_test/WebsiteTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakundo.website_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
