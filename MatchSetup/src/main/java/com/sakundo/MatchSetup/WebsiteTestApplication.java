122c10de2131ecd7bbd805bf4c5ecac9d8e25d0b
IPVA <ivanpereira345@gmail.com>
2025-04-16 18:33:22 -0300

    Resetting everything

--- MatchSetup/src/main/java/com/sakundo/MatchSetup/WebsiteTestApplication.java
+++ MatchSetup/src/main/java/com/sakundo/MatchSetup/WebsiteTestApplication.java
@@ -1,4 +1,4 @@
-package com.sakundo.SetupMatch;
+package com.sakundo.MatchSetup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
